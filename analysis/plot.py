496e20f6e575cf08c23da93c2a9dbf00d8312122
romain.fontugne <romain.fontugne@gmail.com>
2015-12-04 18:34:09 +0900

    clean: separate plot/tool functions

---
+++ analysis/plot.py
@@ -0,0 +1,193 @@
+import matplotlib.pylab as plt
+import tools
+
+
+def sampleVsShapiro(results):
+
+    for i, label in [(0,"Measured"), (1,"Inferred")]:
+        if resutls[i] is None:
+            continue
+
+        plt.figure()
+
+        shapiro = {}
+        x = []
+        y = []
+
+        for k,v in results[i].iteritems():
+            if len(v) > 40:
+                x.append(np.mean(results[i+2][k]))
+                y.append(stats.shapiro(v)[1])
+        
+        if i==0:
+            plt.plot(x,y,'o')
+            plt.title(label)
+
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.xlabel("Avg. # samples")
+        plt.xlabel("Shapiro test")
+        # plt.legend()
+        plt.grid(True)
+        plt.savefig("nbSampleVsShapiro_%s.eps" % label)
+
+
+
+def cLTMeanVsStd(results):
+    
+    # flatten the lists
+    rng=None
+    for i, label in [ (1, "Inferred"), (0, "Measured")]:
+        if resutls[i] is None:
+            continue
+
+        data = results[i].values()
+        #filtering
+        data = filter(lambda x: len(x)>48, data)
+
+        mean = map(np.mean,data) 
+        std = map(np.std,data)
+
+        # mean = np.random.choice(mean, (5000))
+        # std = np.random.choice(std, (5000))
+
+        plt.figure()
+        #rng = [np.min([np.min(measured),np.min(inferred)]), np.max([np.max(measured), np.max(inferred)])]
+        plt.plot(mean,std, "o") 
+        plt.title("Distribution of the sample mean (%s)" % label)
+        plt.grid(True)
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.xlabel("Mean")
+        plt.ylabel("Std. dev.")
+        xlim = plt.xlim()
+        ylim = plt.ylim()
+        minlim = np.max([np.min(xlim),np.min(ylim)])
+        maxlim = np.min([np.max(xlim),np.max(ylim)])
+        plt.plot([minlim, maxlim], [minlim, maxlim], "--k", lw=2)
+        plt.savefig("CLTMeanVsStd_%s.eps" % label)
+
+
+def cLTMedianVsMad(results):
+    
+    # flatten the lists
+    rng=None
+    for i, label in [ (1, "Inferred"), (0, "Measured")]:
+        if resutls[i] is None:
+            continue
+
+        data = results[i].values()
+
+        #filtering
+        data = filter(lambda x: len(x)>48, data)
+
+        mean = map(np.median,data) 
+        std = map(tools.mad,data)
+
+        # mean = np.random.choice(mean, (5000))
+        # std = np.random.choice(std, (5000))
+
+        plt.figure()
+        #rng = [np.min([np.min(measured),np.min(inferred)]), np.max([np.max(measured), np.max(inferred)])]
+        plt.plot(mean,std, "o") 
+        plt.title("Distribution of the sample mean (%s)" % label)
+        plt.grid(True)
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.xlim([10**-4, 10**3])
+        plt.ylim([10**-3, 10**3])
+        plt.xlabel("Median")
+        plt.ylabel("MAD")
+        xlim = plt.xlim()
+        ylim = plt.ylim()
+        minlim = np.max([np.min(xlim),np.min(ylim)])
+        maxlim = np.min([np.max(xlim),np.max(ylim)])
+        plt.plot([minlim, maxlim], [minlim, maxlim], "--k")
+        plt.savefig("CLTMedianVsMad_%s.eps" % label)
+
+# def distributionCLTStd(results):
+    
+    # # flatten the lists
+    # measured = map(np.std,results[0].values()) 
+    # inferred = map(np.std,results[1].values()) 
+    # rng=None
+    # for data, label in [(measured, "Measured"), (inferred, "Inferred")]:
+        # plt.figure()
+        # #rng = [np.min([np.min(measured),np.min(inferred)]), np.max([np.max(measured), np.max(inferred)])]
+        # plt.hist(data, bins=50,  label=label, range=rng) 
+        # plt.legend()
+        # plt.grid(True)
+        # plt.xscale("log")
+        # plt.yscale("log")
+        # plt.savefig("distriutionCLTStd_%s.eps" % label)
+
+# def distributionCLTMean(results):
+    
+    # # flatten the lists
+    # measured = map(np.mean,results[0].values()) 
+    # inferred = map(np.mean,results[1].values()) 
+    # rng=None
+    # for data, label in [(measured, "Measured"), (inferred, "Inferred")]:
+        # plt.figure()
+        # #rng = [np.min([np.min(measured),np.min(inferred)]), np.max([np.max(measured), np.max(inferred)])]
+        # plt.hist(data, bins=50,  label=label, range=rng) 
+        # plt.legend()
+        # plt.grid(True)
+        # plt.xscale("log")
+        # plt.yscale("log")
+        # plt.savefig("distriutionCLTMean_%s.eps" % label)
+
+
+def distributionNbSamples(results):
+    
+    # flatten the lists
+    measured = list(itertools.chain.from_iterable(results[2].values()))
+    inferred = list(itertools.chain.from_iterable(results[3].values()))
+    rng = [0, 8000] 
+    for data, label in [(measured, "Measured"), (inferred, "Inferred")]:
+        plt.figure()
+        plt.hist(data, bins=50,  label=label, range=rng)
+        plt.legend()
+        plt.grid(True)
+        #plt.xscale("log")
+        plt.yscale("log")
+        plt.title("Mean = %s" % np.mean(data))
+        plt.savefig("distriutionNbSamples_%s.eps" % label)
+
+def distributionAvgNbSamples(results):
+    
+    measured = filter(lambda x: len(x)>96, results[2].values())
+    inferred  = filter(lambda x: len(x)>96, results[3].values())
+    # flatten the lists
+    measured = map(np.mean,measured)
+    inferred = map(np.mean,inferred)
+    rng = None #[3, 204] 
+    for data, label in [(measured, "Measured"), (inferred, "Inferred")]:
+        plt.figure()
+        #rng = [np.min([np.min(measured),np.min(inferred)]), np.max([np.max(measured), np.max(inferred)])]
+        plt.hist(data, bins=500,  label=label, range=rng)
+        plt.legend()
+        plt.grid(True)
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.title("Mean = %s" % np.mean(data))
+        plt.savefig("distriutionAvgNbSamples_%s.eps" % label)
+
+def distributionShapiro(results):
+
+    for data, label, samples in [(results[0], "Measured", results[2]), (results[1], "Inferred", results[3])]:
+        plt.figure()
+        data0 = []
+        for k,v in data.iteritems():
+            if len(v)>96:
+                data0.append(stats.shapiro(v)[1])
+
+
+        plt.hist(data0, bins=100, label=label)
+        plt.legend()
+        plt.grid(True)
+        plt.yscale("log")
+        plt.xscale("log")
+        plt.xlabel("Shapiro p-value")
+        plt.title("Mean = %0.3f" % np.mean(data0))
+        plt.savefig("distriutionShapiro_%s.eps" % label)
---
+++ analysis/tools.py
@@ -0,0 +1,22 @@
+import re
+import numpy as np
+
+# https://en.wikipedia.org/wiki/Private_network
+priv_lo = re.compile("^127\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+priv_24 = re.compile("^10\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+priv_20 = re.compile("^192\.168\.\d{1,3}.\d{1,3}$")
+priv_16 = re.compile("^172.(1[6-9]|2[0-9]|3[0-1]).[0-9]{1,3}.[0-9]{1,3}$")
+
+def isPrivateIP(ip):
+
+    return priv_lo.match(ip) or priv_24.match(ip) or priv_20.match(ip) or priv_16.match(ip)
+
+
+def mad(arr):
+    """ Median Absolute Deviation: a "Robust" version of standard deviation.
+        Indices variabililty of the sample.
+        https://en.wikipedia.org/wiki/Median_absolute_deviation 
+    """
+    arr = np.ma.array(arr).compressed() # should be faster to not use masked arrays.
+    med = np.median(arr)
+    return np.median(np.abs(arr - med))
