f073e8fbbb04014f8e74fabea65e0f2cc7ee8d2b
romain.fontugne <romain.fontugne@gmail.com>
2016-01-29 12:37:47 +0900

    cleaned ripe downloading and mongo importing scripts

--- dataManipulation/downloadData.py
+++ dataManipulation/downloadData.py
@@ -1,81 +1,95 @@
 import os 
 from datetime import datetime
 from datetime import timedelta
+from datetime import date
 from ripe.atlas.cousteau import AtlasResultsRequest 
 import json
 import pymongo
 import gzip
 
 
-# dates
-start = datetime(2015, 12, 29, 0, 0)
-end = datetime(2016, 1, 1, 0, 0)
-# end = datetime(2016, 1, 1, 0, 0)
-timeWindow = timedelta(minutes=60)
-
-# Measurments IDs
-builtinIdv4 = range(5001,5027)
-# builtinIdv6 = range(6001,6022)
-
-errors = []
-
-# storage = "mongo"
-storage = "mongo"
-if storage == "mongo":
-    client = pymongo.MongoClient("mongodb-iijlab")
-    db = client.atlas
-
-# Get measurments results
-for msmId in builtinIdv4:
-
-    currDate = start
-    while currDate+timeWindow<end:
-        path = "../data/%s/%s" % (currDate.year, currDate.month)
-        try:
-            print("%s:  measurement id %s" % (currDate, msmId) )
-            if not os.path.exists(path):
-                os.makedirs(path)
-            if os.path.exists("%s/%s_msmId%s.json.gz" % (path, currDate, msmId)):
-                continue
-
-            kwargs = {
-                "msm_id": msmId,
-                "start": currDate,
-                "stop": currDate+timeWindow,
-                # "probe_ids": [1,2,3,4]
-            }
-
-            is_success, results = AtlasResultsRequest(**kwargs).create()
-
-            if is_success :
-                    results = list(results)
-                # else:
-                    # Output file
-                    fi = gzip.open("%s/%s_msmId%s.json.gz" % (path, currDate, msmId) ,"wb")
-                    print("Storing data for %s measurement id %s" % (currDate, msmId) )
-                    json.dump(results, fi)
-                    fi.close()
-                # if storage == "mongo":
-                    if len(results)==0:
-                        continue
-                    print("Sending data to Mongodb server")
-                    # collection = "traceroute6_%s_%02d_%02d" % (currDate.year, 
-                    collection = "traceroute_%s_%02d_%02d" % (currDate.year, 
-                                                currDate.month, currDate.day)
-                    col = db[collection]
-                    col.insert_many(results)
-                    col.create_index("timestamp", background=True)       
-
-            else:
-                errors.append("%s: msmId=%s" % (currDate, msmId))
-
-        except ValueError:
-            errors.append("%s: msmId=%s" % (currDate, msmId))
-
-        finally:
-            currDate += timeWindow
 
 
-if errors:
-    print("Errors with the following parameters:")
-    print(errors)
+downloadData(start, end, msmTypes = ["builtin", "anchor"], afs = [4, 6], timeWindow = timedelta(minutes=60)):
+
+        
+    errors = []
+
+    # storage = "mongo"
+    storage = "mongo"
+    if storage == "mongo":
+        client = pymongo.MongoClient("mongodb-iijlab")
+        db = client.atlas
+
+    # Get measurments results
+    for af in afs:
+        for msmType in msmTypes:
+            # Read the corresponding id file
+            for line in  open("./%sMsmIdsv%s.txt" % (msmType, af), "r"):
+                msmId = int(line.partition(":")[2])
+
+                currDate = start
+                while currDate+timeWindow<end:
+                    path = "../data/ipv%s/%s/%s/%s" % (af,msmType,currDate.year, currDate.month)
+                    try:
+                        print("%s:  measurement id %s" % (currDate, msmId) )
+                        if not os.path.exists(path):
+                            os.makedirs(path)
+                        if os.path.exists("%s/%s_msmId%s.json.gz" % (path, currDate, msmId)):
+                            continue
+
+                        kwargs = {
+                            "msm_id": msmId,
+                            "start": currDate,
+                            "stop": currDate+timeWindow,
+                            # "probe_ids": [1,2,3,4]
+                        }
+
+                        is_success, results = AtlasResultsRequest(**kwargs).create()
+
+                        if is_success :
+                                results = list(results)
+                            # else:
+                                # Output file
+                                fi = gzip.open("%s/%s_msmId%s.json.gz" % (path, currDate, msmId) ,"wb")
+                                print("Storing data for %s measurement id %s" % (currDate, msmId) )
+                                json.dump(results, fi)
+                                fi.close()
+                            # if storage == "mongo":
+                                if len(results)==0:
+                                    continue
+                                print("Sending data to Mongodb server")
+                                if af == 4:
+                                    #TODO store data in traceroute4
+                                    collection = "traceroute_%s_%02d_%02d" % (currDate.year, 
+                                                                currDate.month, currDate.day)
+                                elif af == 6:
+                                    collection = "traceroute6_%s_%02d_%02d" % (currDate.year, 
+                                                                currDate.month, currDate.day)
+                                else:
+                                    None
+
+                                col = db[collection]
+                                col.insert_many(results)
+                                col.create_index("timestamp", background=True)       
+
+                        else:
+                            errors.append("%s: msmId=%s" % (currDate, msmId))
+
+                    except ValueError:
+                        errors.append("%s: msmId=%s" % (currDate, msmId))
+
+                    finally:
+                        currDate += timeWindow
+
+
+    if errors:
+        print("Errors with the following parameters:")
+        print(errors)
+
+
+if __name__ == "__main__":
+    # download yesterday's data
+    yesterday = date.today() - timedelta(days=1)
+
+    downloadData(yesterday, date.today())
