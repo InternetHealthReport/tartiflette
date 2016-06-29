b1ab9e2c6e92af5e708445506d1270e3e4872133
Romain Fontugne <romain@iij.ad.jp>
2016-06-29 11:44:38 +0900

    small fix in stream.py

--- dataManipulation/stream.py
+++ dataManipulation/stream.py
@@ -4,6 +4,7 @@ import textwrap
 import smtplib
 import datetime
 import emailConf
+import time
 
 client = pymongo.MongoClient("mongodb-iijlab")
 db = client.atlas
@@ -16,9 +17,9 @@ def sendMail(message):
     msg = textwrap.dedent("""\
         From: %s 
         To: %s 
-        Subject: Atlas stream stopped! 
+        Subject: Atlas stream stopped on %s (UTC)! 
         %s 
-    """ % (emailConf.orig, ",".join(emailConf.dest), message))
+    """ % (emailConf.orig, ",".join(emailConf.dest), datetime.datetime.utcnow(), message))
 
     # Send the mail
     server = smtplib.SMTP(emailConf.server)
@@ -54,35 +55,29 @@ def on_result_response(*args):
 if __name__ == "__main__":
 
     try:
-        atlas_stream = AtlasStream()
-        atlas_stream.connect()
-        # Measurement results
-        channel = "result"
-        # Bind function we want to run with every result message received
-        atlas_stream.bind_channel(channel, on_result_response)
-        # Subscribe to new stream for 1001 measurement results
-        stream_parameters = {"type": "traceroute"}
-        atlas_stream.start_stream(stream_type="result", **stream_parameters)
+        while True:
+            atlas_stream = AtlasStream()
+            atlas_stream.connect()
+            # Measurement results
+            channel = "result"
+            # Bind function we want to run with every result message received
+            atlas_stream.bind_channel(channel, on_result_response)
+            # Subscribe to new stream 
+            stream_parameters = {"type": "traceroute"}
+            atlas_stream.start_stream(stream_type="result", **stream_parameters)
 
-        # Probe's connection status results
-        # channel = "probe"
-        # atlas_stream.bind_channel(channel, on_result_response)
-        # stream_parameters = {"enrichProbes": True}
-        # atlas_stream.start_stream(stream_type="probestatus", **stream_parameters)
+            # Run forever
+            atlas_stream.timeout(seconds=None)
+            # Shut down everything
+            atlas_stream.disconnect()
 
-        # Timeout all subscriptions after 5 secs. Leave seconds empty for no timeout.
-        # Make sure you have this line after you start *all* your streams
-        atlas_stream.timeout(seconds=10)
-        # Shut down everything
-        atlas_stream.disconnect()
-
-        print "end of the script"
+            time.sleep(1) 
 
         # This script should never end, send an email if it does
         save_note = "normal exit"
     except Exception as e: 
         save_note = "Exception dump: %s : %s." % (type(e).__name__, e)
     finally:
-        exception_fp = open("dump_%s.err" % datetime.datetime.now(), "w")
+        exception_fp = open("dump_%sUTC.err" % datetime.datetime.utcnow(), "w")
         exception_fp.write(save_note) 
         sendMail(save_note)
