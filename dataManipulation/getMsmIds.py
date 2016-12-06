b5998944a230919fe8bce0e60bc89fb3f954fb0f
romain.fontugne <romain.fontugne@gmail.com>
2016-12-06 19:26:52 +0900

    added more msm ids

---
+++ dataManipulation/getMsmIds.py
@@ -0,0 +1,19 @@
+from ripe.atlas.cousteau import MeasurementRequest
+from collections import defaultdict
+
+filters = {"status": 2, "type": "traceroute", "af": 4}
+measurements = MeasurementRequest(**filters)
+
+count = 0
+stat = {}
+msmIds = []
+for msm in measurements:
+    if msm["interval"] <= 1800 and (msm["participant_count"] is None or msm["participant_count"]>100):
+        # for k,v in msm.
+        print(msm)
+        msmIds.append(msm["id"])
+        count += 1
+
+# Print total count of found measurements
+print("Found %s interesting measurement out of %s" % (count, measurements.total_count))
+print list(set(msmIds))
